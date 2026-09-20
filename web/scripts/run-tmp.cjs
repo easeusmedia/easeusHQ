6287e31fab5908f7af8ffa086c5ec4ee77a73d18
Abhishek <team.easeusnow@gmail.com>
2026-09-20 14:49:58 +0530

    Drop a stray scratch script

--- web/scripts/run-tmp.cjs
+++
@@ -1,3 +0,0 @@
-const { createJiti } = require("jiti");
-const j = createJiti(__filename, { alias: { "@": require("path").resolve("src") } });
-j.import("./" + process.argv[2]).catch((e) => { console.error(e); process.exit(1); });
